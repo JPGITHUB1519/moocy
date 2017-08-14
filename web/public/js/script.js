d6981e465bae7eafe61f033a32a7a7bc01ea3d97
Jean Urena <juanpedro1519@gmail.com>
2017-08-14 20:21:40 -0300

    Add database and README

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,3 +1,9 @@
+/*
+    Get a variable passed on the get parameters
+    @parameterName string -> Key-Value containing his key and the description
+    @return -> value of the key
+*/
+
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -11,13 +17,13 @@ function findGetParameter(parameterName) {
     return result;
 }
 
-$( document ).ready(function() {
+// logic to get the value for the video and modify the source tag
+$(document).ready(function() {
     var video_elem = $("#video"); 
     var video_source = $("#source");
     var src = findGetParameter("video");
     video_source.attr("src", src);
     console.log(video_source.attr("src"));
     video_elem[0].load();
-    // video_elem.play();
 });
     
